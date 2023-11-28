3e201a54424153cc8a9c5f737d5fc9e556d08b5f
Nihadname <nihadcoding@gmail.com>
2023-11-28 19:56:01 +0400

    Add project files.

---
+++ ConsoleApp23/Models/Fahrenheit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23.Models
+{
+    internal class Fahrenheit
+    {
+        public double fahrenheit {  get; set; }
+        public Fahrenheit(double fahrenheits) { 
+        fahrenheit = fahrenheits;
+        }
+    }
+}
---
+++ ConsoleApp23/Models/celcius.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23.Models
+{
+    internal class celcius
+    {
+        public double Celcius;
+        public celcius(double Celciuss)
+        {
+            Celcius = Celciuss;
+        }
+        public static implicit operator Fahrenheit(celcius s)
+        {
+            return new Fahrenheit(s.Celcius/33.8);
+        }
+        
+
+        }
+    }
+
